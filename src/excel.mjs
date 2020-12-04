45d672bbb4fce901a1f4b7d018f62e630adff157
Chung Leong <chernyshevsky@hotmail.com>
2020-12-04 18:14:48 +0100

    Added skeleton Excel classes.

---
+++ src/excel.mjs
@@ -0,0 +1,69 @@
+class Cell {
+  get text() {
+  }
+
+  get richText() {
+  }
+
+  get image() {
+  }
+
+  get value() {
+  }
+
+  get style() {
+  }
+
+  get(type) {
+
+  }
+
+  render(props) {
+
+  }
+}
+
+class Row {
+  get object() {
+  }
+
+  get cells() {
+  }
+
+  get(types) {
+  }
+
+  render(props) {
+  }
+}
+
+class Column {
+  get hearer() {
+  }
+
+  get cells() {
+  }
+
+  get(type) {
+  }
+
+  render(props) {
+  }
+}
+
+class Sheet {
+  get objects() {
+  }
+
+  get rows() {
+  }
+
+  get columns() {
+  }
+
+  get(types) {
+  }
+
+  render(props) {
+  }
+}
